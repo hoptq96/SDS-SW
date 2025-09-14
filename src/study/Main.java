580bc2d6a64634a28090eb4edeff70016875339d
HopTQ <quanghop.uneti@gmail.com>
2025-09-14 22:25:05 +0700

    commit 2.4

--- src/study/Main.java
+++ src/study/Main.java
@@ -3,6 +3,7 @@ package study;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Testt");
+        int a = 0;
         System.out.println("Test");
         System.out.println("Test");
     }
