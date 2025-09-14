e4a80bad868cff3e5aed3a8d74058f1bf8af75f3
HopTQ <quanghop.uneti@gmail.com>
2025-09-14 22:21:13 +0700

    commit 2.3

--- src/study/Main.java
+++ src/study/Main.java
@@ -2,11 +2,6 @@ package study;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testt");
-        System.out.println("Test");
-        System.out.println("Test");
-        int b = 0;
-        int a = 1;
         System.out.println("Test");
         System.out.println("Test");
         int b = 0;
