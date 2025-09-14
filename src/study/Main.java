3c4573fb6bf215326d2cec87764c9cfa892b240c
HopTQ <quanghop.uneti@gmail.com>
2025-09-14 22:30:05 +0700

    commit 3

--- src/study/Main.java
+++ src/study/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test");
-        int b = 0;
         int a = 1;
+        int b = 0;
         System.out.println("Test");
+        int c = 1;
     }
 }
