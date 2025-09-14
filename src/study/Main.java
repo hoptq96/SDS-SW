f60b8ed6b30792cbba0ae36f253e94c644d6d0ad
HopTQ <quanghop.uneti@gmail.com>
2025-09-14 22:21:13 +0700

    commit master rebase 3

--- src/study/Main.java
+++ src/study/Main.java
@@ -4,9 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testt");
         System.out.println("Test");
-
-        System.out.println("Test");
-        System.out.println("Test");
         System.out.println("Test");
         int b = 0;
         int a = 1;
