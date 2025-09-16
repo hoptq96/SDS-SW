9ed83cc04ee14589f30b363ec13c5ec0877e7896
hoptq96 <48143123+hoptq96@users.noreply.github.com>
2025-09-16 21:08:53 +0700

    Update Main.java

--- src/study/Main.java
+++ src/study/Main.java
@@ -5,7 +5,10 @@ public class Main {
         System.out.println("Test");
         int a = 1;
         int b = 0;
-
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
         int c = 1;
         int d = 1;
         System.out.println("Test");
