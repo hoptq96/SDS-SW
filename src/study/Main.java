68d756f8356be84e2f5a8d4357261d00315b20d0
HopTQ <quanghop.uneti@gmail.com>
2025-09-16 21:07:55 +0700

    commit F1.5

--- src/study/Main.java
+++ src/study/Main.java
@@ -8,5 +8,10 @@ public class Main {
         int c = 1;
         int d = 1;
         System.out.println("Test");
+        System.out.println("Test2");
+        System.out.println("Test3");
+        System.out.println("Test4");
+        System.out.println("Test5");
+        System.out.println("Test6");
     }
 }
