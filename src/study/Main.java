fdf85ed48ce302eedff418670d26d3ec189d023f
hoptq96 <48143123+hoptq96@users.noreply.github.com>
2025-09-16 21:09:14 +0700

    Update Main.java

--- src/study/Main.java
+++ src/study/Main.java
@@ -12,5 +12,7 @@ public class Main {
         int c = 1;
         int d = 1;
         System.out.println("Test");
+        System.out.println("Test2");
+        System.out.println("Test3");
     }
 }
